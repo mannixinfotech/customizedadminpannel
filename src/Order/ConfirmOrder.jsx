7a8ce13105b3f8774de26e1eb3a5b77f4ff57697
mannixinfotech <168402614+mannixinfotech@users.noreply.github.com>
2024-10-18 12:55:22 +0530

    Update ConfirmOrder.jsx

--- src/Order/ConfirmOrder.jsx
+++ src/Order/ConfirmOrder.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState }  from 'react'
 import SideBar from '../Componets/SideBar'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { toast, ToastContainer } from 'react-toastify';
 
 import DataTable from 'react-data-table-component';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import axios from 'axios';
 import DownloadIcon from "@mui/icons-material/Download";
-
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import PrintIcon from '@mui/icons-material/Print';
 import { useNavigate } from 'react-router-dom';
 
@@ -15,7 +16,8 @@ const ConfirmOrder = () => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [loading, setLoading] = useState(true);
- 
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [deleteId, setDeleteId] = useState(null);
   const [searchQuery, setSearchQuery] = useState("");
   
 const navigate = useNavigate();
@@ -60,7 +62,29 @@ const navigate = useNavigate();
     {
       navigate(`/print-bill/${id}`);
     }
-  
+    const handleDelete = (row) => {
+      setDeleteId(row._id);
+      setShowDeleteDialog(true);
+    };
+      const confirmDelete = () => {
+          axios
+            .delete(`https://customizedapi.onrender.com/delete/${deleteId}`)
+            .then((response) => {
+              if (response.status === 200) {
+                fetchProducts();
+                setShowDeleteDialog(false);
+                toast.success("order deleted successfully!");
+              }
+            })
+            .catch((error) => {
+              console.error("There was an error deleting the product!", error);
+              setShowDeleteDialog(false);
+            });
+        };
+      const cancelDelete = () => {
+          setShowDeleteDialog(false);
+          setDeleteId(null);
+        };
   
     const columns = [
       {
@@ -127,6 +151,12 @@ const navigate = useNavigate();
             >
                 <PrintIcon className="text-xl border border-red-500" />
             </button>
+            <button
+                onClick={() => handleDelete(row)}
+                className="text-red-600 hover:text-red-800"
+              >
+                <FontAwesomeIcon icon={faTrashAlt} className="text-xl" />
+              </button>
           </div>
         ),
       },
@@ -192,6 +222,29 @@ const navigate = useNavigate();
         
        
       </div>
+      {showDeleteDialog && (
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="bg-gray-100 p-6 rounded shadow-lg">
+             
+              <p>Are you sure you want to delete this user data?</p>
+              <div className="mt-4 flex space-x-4 justify-center">
+                <button
+                  onClick={confirmDelete}
+                  className="bg-red-500 text-white px-4 py-2 rounded"
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={cancelDelete}
+                  className="bg-gray-500 text-white px-4 py-2 rounded"
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      <ToastContainer/>
     </div>
     
     
