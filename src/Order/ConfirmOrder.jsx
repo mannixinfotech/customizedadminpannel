3ccd9898ed7c8c3ecaf04ac56b8f3314ac1dc12b
mannixinfotech <168402614+mannixinfotech@users.noreply.github.com>
2024-10-18 13:16:47 +0530

    Update ConfirmOrder.jsx

--- src/Order/ConfirmOrder.jsx
+++ src/Order/ConfirmOrder.jsx
@@ -68,7 +68,7 @@ const navigate = useNavigate();
     };
       const confirmDelete = () => {
           axios
-            .delete(`https://customizedapi.onrender.com/delete/${deleteId}`)
+            .delete(`https://customizedapi.onrender.com/order/delete/${deleteId}`)
             .then((response) => {
               if (response.status === 200) {
                 fetchProducts();
